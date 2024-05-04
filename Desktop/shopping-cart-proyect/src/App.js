d552cb430c0c9f309172a0c3eab6e60465eac327
Ivo Konstantinow <ivokonstantinow@gmail.com>
2024-05-04 18:24:16 -0300

    shopping-cart-proyect installing dependencies of MUI and adding card component

---
+++ Desktop/shopping-cart-proyect/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Product from './components/Product';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Proyecto react-cart-shop</h1>
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
