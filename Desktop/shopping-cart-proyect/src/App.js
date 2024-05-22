2bb7885a266190b03c143d7d1f60dc67d2fe8694
Ivo Konstantinow <ivokonstantinow@gmail.com>
2024-05-22 16:38:02 -0300

    CheckOutPage component desing

--- Desktop/shopping-cart-proyect/src/App.js
+++ Desktop/shopping-cart-proyect/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import React from 'react';
-import Product from './components/Product/Product';
 import Navbar from './components/Navbar/Navbar'
 import Products from './components/Products';
-
+import CheckOutPage from './components/CheckOutPage'
 
 function App() {
   return (
     <>
       <Navbar/>
       <Products/>
+      <CheckOutPage/>
       {/* <Product/> */}
     </>
   );
--- Desktop/shopping-cart-proyect/src/components/Products.js
+++ Desktop/shopping-cart-proyect/src/components/Products.js
@@ -1,18 +1,9 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Product from './Product/Product';
 import products from './product-data'
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
 
 export default function Products() {
 return (
