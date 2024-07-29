60464bf48e442b8f903d3368e85e00feed314ee8
Ivo Konstantinow <ivokonstantinow@gmail.com>
2024-07-29 15:07:04 -0300

    installing and creating tailwind component

---
+++ Desktop/noble_by_vue_proyect/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: ['/index.htnml', '.src/**/*.{vue,js,ts,jsx,tsx'],
+
+    darkmode: false,
+
+    theme: {
+        extend: {},
+    },
+
+    variants: {
+        extend: {}
+    },
+
+    plugins: [],
+}
