14faf2754d0d04b753d1359152f8f53d7b1ba7ab
Ivo Konstantinow <ivokonstantinow@gmail.com>
2025-06-05 21:40:23 -0300

    fixing duplicate files..

--- Desktop/iniciacion_a_la_programacion_-_DB-main/menu.py
+++ Desktop/iniciacion_a_la_programacion_-_DB-main/menu.py
@@ -1,6 +1,9 @@
 from products import cargar_productos, ver_tabla_productos, modificar_producto, borrar_producto
 
 def menu_admin(admin):
+    
+    from main import main
+    
     opciones = {
         "1": admin.mostrar_info,
         "2": cargar_productos,
@@ -16,18 +19,20 @@ def menu_admin(admin):
         print("3. Ver tabla de productos")
         print("4. Modificar producto")
         print("5. Eliminar producto")
-        print("6. Volver al menú principal")
+        print("6. Salir del menú administrador")
         opcion = input("Elige una opción: ").strip()
 
         if opcion == "6":
             print("🔁 Volviendo al menú principal...\n")
-            return  # 👈 vuelve al bucle de main()
+            main()
         elif opcion in opciones:
             opciones[opcion]()
         else:
             print("❌ Opción inválida. Intenta de nuevo.\n")
 
 def menu_cliente(cliente):
+    from main import main
+    
     opciones = {
         "1": cliente.mostrar_info,
         "2": ver_tabla_productos
@@ -37,12 +42,12 @@ def menu_cliente(cliente):
         print("\nMenú Cliente:")
         print("1. Ver mis datos")
         print("2. Ver tabla de productos")
-        print("3. Volver al menú principal")
+        print("3. Salir")
         opcion = input("Elige una opción: ").strip()
 
         if opcion == "3":
             print("🔁 Volviendo al menú principal...\n")
-            return  # 👈 vuelve al bucle de main()
+            main()
         elif opcion in opciones:
             opciones[opcion]()
         else:
